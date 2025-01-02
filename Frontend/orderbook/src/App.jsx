a0b20b8ef6f302e27d42b7ad769ac13491b49aa5
Aamleen Ahmed <aamleen20002@iiitd.ac.in>
2025-01-02 12:06:01 +0530

    Login, Logout and Register created and tested.

--- Frontend/orderbook/src/App.jsx
+++ Frontend/orderbook/src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react'
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
-
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
